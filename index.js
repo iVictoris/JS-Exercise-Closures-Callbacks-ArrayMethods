4806bb2cecb6565eba13d0484d3c58b1af773734
Victor Tran <victor.n.tran@outlook.com>
2019-11-18 11:48:20 -0800

    complete firstNamesAllCaps

--- index.js
+++ index.js
@@ -208,8 +208,9 @@ function getFullNames(runners = []) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
+function firstNamesAllCaps(runners = []) {
   /* CODE HERE */
+  return runners.map(({first_name}) => first_name.toUpperCase());
 }
 
 /**
