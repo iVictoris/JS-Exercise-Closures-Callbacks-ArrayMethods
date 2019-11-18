6bdfe772b715628ee5c6f088743bb3e04b2ea120
Victor Tran <victor.n.tran@outlook.com>
2019-11-18 11:54:46 -0800

    complete counterMaker

--- index.js
+++ index.js
@@ -266,11 +266,12 @@ function tallyUpDonations(runners = []) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
+  let count = 0;
   function counter() {
-    ++count
+    return count++
   }
   // BROKEN CODE ENDS
+  return counter;
 }
 
 /**
